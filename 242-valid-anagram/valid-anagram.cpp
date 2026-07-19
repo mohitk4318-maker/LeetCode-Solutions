7148cff2f559ca34cfbec7aa7ebbaab64c2710ef
Mohit Singh Rathore <mohitk4318@gmail.com>
2026-07-19 18:51:58 +0530

    Time: 5 ms (26.19%) | Memory: 9.6 MB (92.90%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(begin(s),end(s));
+        sort(begin(t),end(t));
+
+        if(s==t)
+        return true;
+
+        return false;
+    }
+};
