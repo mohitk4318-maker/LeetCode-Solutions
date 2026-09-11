4084dbbe9e5330f66d1e13622224e688fc95412e
Mohit Singh Rathore <mohitk4318@gmail.com>
2026-09-11 22:32:33 +0530

    Time: 15 ms (46.82%) | Memory: 42.4 MB (56.55%) - LeetSync

--- 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
+++ 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
- typedef long long ll;
- ll findcost(vector<int>& nums, vector<int>& cost,int target){
+typedef long long ll;
+
+ll findcost(vector<int>& nums, vector<int>& cost,int target){
 
     ll result=0;
-
     for(int i=0;i<nums.size();i++){
-        result =result + (ll) abs(nums[i]-target) * cost[i];
+        result += (ll)abs(nums[i] - target) * cost[i];
     }
-
     return result;
- }
-
+}
     long long minCost(vector<int>& nums, vector<int>& cost) {
-        ll answer=INT_MAX;
+        ll answer =INT_MAX;
 
-        ll left=*min_element(begin(nums),end(nums));
-         ll right=*max_element(begin(nums),end(nums));
+        int left=*min_element(begin(nums),end(nums));
+        int right=*max_element(begin(nums),end(nums));
 
-         while(left<=right){
+        while(left<=right){
 
-            int mid=left+(right-left)/2;
+            int mid= left+(right-left)/2;
 
             ll cost1=findcost(nums,cost,mid);
             ll cost2=findcost(nums,cost,mid+1);
 
-            answer=min(cost1 , cost2);
+            answer=min(cost1,cost2);
 
-            if(cost2>cost1){
+            if(cost1<cost2){
                 right=mid-1;
             }
-            else{
-                left=mid+1;
-            }
-
-         }
-
-         return answer==INT_MAX ? 0 : answer;
-       
+            else
+            left=mid+1;
+        }
+        return answer  == INT_MAX ? 0 : answer;
     }
 };
