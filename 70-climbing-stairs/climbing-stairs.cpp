65f46ebe0bf06eecc050b85ac8aa233095952add
Mohit Singh Rathore <mohitk4318@gmail.com>
2026-09-20 00:41:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (73.53%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-int solve(int n,vector<int> &dp){
-    if(n<0){
-        return 0;
-    }
 
-    if(n == 0){
-        return 1;
-    }
-
-    if(dp[n] != -1){
-        return dp[n];
-    }
-
-    int left_step=solve(n-1,dp);
-    int right_step=solve(n-2,dp);
-
-    return dp[n]=left_step + right_step;
-}
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+
+        if(n ==1 || n==2 ){
+            return n;
+        }
+
+        int a=1;
+        int b=2;
+        int c=3;
+
+        for(int i=3;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+            
+        }
+        return c;
     }
 };
