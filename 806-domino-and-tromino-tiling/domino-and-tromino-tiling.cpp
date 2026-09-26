0b08cc157afd93a132dc967ae8da314ae49a1045
Mohit Singh Rathore <mohitk4318@gmail.com>
2026-09-26 15:51:05 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (79.26%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+int dp[1001];
+int m=1000000007;
+
+int solve(int n){
+    if(n==1 || n==2){
+        return n;
+    }
+
+    if(n==3){
+        return 5;
+    }
+
+    if(dp[n] != -1){
+        return dp[n];
+    }
+
+    return dp[n] = (2 * solve(n-1)%m + solve(n-3)%m) % m;
+}
+
+    int numTilings(int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+};
